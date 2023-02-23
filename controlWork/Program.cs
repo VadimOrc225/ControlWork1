7f4b16a8654a714cfb9e9bdee9f5d8e28c84466f
Vadim <orc225@mail.ru>
2023-02-23 15:45:16 +0300

    initial commit

---
+++ controlWork/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] arr = new string[] {"one", "two", "three", "four", "five", "ff"};
+string[] arr2 = new string[6];
+for (int x=0; x<arr.Length; x++)
+{
+    int count = 0;
+    if (arr[x].Length<4) 
+    {
+       arr2[count] = arr[x];
+       count++;
+       Console.Write($"{arr[x]} ");
+    }
+}
